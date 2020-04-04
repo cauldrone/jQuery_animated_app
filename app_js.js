3d1cae586fb4917e4fa3f454901a29bf7c82ce60
general135 <derpyderps135@gmail.com>
2020-04-03 19:02:41 -0700

    movement is more precise

--- app_js.js
+++ app_js.js
@@ -1,8 +1,11 @@
 
 $(document).ready(() => {
    $("#button").click(() => {
-    $("svg").animate({
-        left: 300
-    })
+    $("svg")
+       .animate({ left: 400})
+       .animate({ bottom: 50})
+       .animate({ right: 800})
+       .animate({ top: 50})
+       .animate({ left: 400})
 })
 })
