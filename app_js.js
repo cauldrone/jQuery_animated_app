9fc407382bcc75b0f1659631e48bdbf900b3fc37
general135 <derpyderps135@gmail.com>
2020-04-04 13:13:56 -0700

    made my code up to date

--- app_js.js
+++ app_js.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function() {
    $("#button").click(function() {
     $("#redball")
@@ -8,3 +9,28 @@ $(document).ready(function() {
        .animate({ left: '400'},2000)
 })
 })
+=======
+$(document).ready(() => {
+    $('body').keyup(function(e){
+   if(e.keyCode == 32){
+       $("#redball").stop();
+   }
+});
+
+  function loop() {
+    $("#redball").css({ left: 0 });
+    $("#redball").animate(
+      {left: "+=250"},1000,"linear"
+    );
+    $("#redball").animate(
+      {left: "-=500"},2000,"linear"
+    );
+    $("#redball").animate(
+      {left: "+=250"},1000,"linear",function () {
+        loop();
+      }
+    );
+  }
+  loop();
+});
+>>>>>>> origin/master
