a5513ac0edfb1c19c03372e441301fd39e0deca5
Em Calderon <calderonhome@Ems-iMac.local>
2020-04-04 13:15:25 -0700

    uhh

--- app_js.js
+++ app_js.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-   $("#button").click(function() {
-    $("#redball")
-       .animate({ left: '400'},2000)
-       .animate({ bottom: '50'},2000)
-       .animate({ right: '800'},2000)
-       .animate({ top: '50'},2000)
-       .animate({ left: '400'},2000)
-})
-})
-=======
 $(document).ready(() => {
     $('body').keyup(function(e){
    if(e.keyCode == 32){
@@ -35,4 +23,4 @@ $(document).ready(() => {
   loop();
     });
 });
->>>>>>> origin/master
+
