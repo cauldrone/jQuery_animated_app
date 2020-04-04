738d2ed438fa03e4576ab5f81100f051e6ac7478
general135 <derpyderps135@gmail.com>
2020-04-03 19:30:27 -0700

    rewording the code

--- app_js.js
+++ app_js.js
@@ -1,11 +1,11 @@
 
-$(document).ready(() => {
-   $("#button").click(() => {
-    $("svg")
-       .animate({ left: 400})
-       .animate({ bottom: 50})
-       .animate({ right: 800})
-       .animate({ top: 50})
-       .animate({ left: 400})
+$(document).ready(function() {
+   $("#button").click(function() {
+    $("#redball")
+       .animate({ left: '400'},2000)
+       .animate({ bottom: '50'},2000)
+       .animate({ right: '800'},2000)
+       .animate({ top: '50'},2000)
+       .animate({ left: '400'},2000)
 })
 })
