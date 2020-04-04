b7b995cfab8c116aaac31003b0fc1cc9683c0c46
Em Calderon <calderonhome@Ems-iMac.local>
2020-04-04 13:08:56 -0700

    loop works (stop button not so much)

--- app_js.js
+++ app_js.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-    $("#button").click(
-    function loop() {
-        $("#redball")
-            .animate({right: "249"}, 1000)
-            .animate({left: "249"},2000)
-    }
-    );
+$(document).ready(() => {
     $('body').keyup(function(e){
    if(e.keyCode == 32){
        $("#redball").stop();
    }
 });
+
+  function loop() {
+    $("#redball").css({ left: 0 });
+    $("#redball").animate(
+      {left: "+=250"},1000,"linear"
+    );
+    $("#redball").animate(
+      {left: "-=500"},2000,"linear"
+    );
+    $("#redball").animate(
+      {left: "+=250"},1000,"linear",function () {
+        loop();
+      }
+    );
+  }
+  loop();
 });
