9b230365744370e56d8ec4cb474893934e401956
Em Calderon <calderonhome@Ems-iMac.local>
2020-04-03 20:00:55 -0700

    move right and left

--- app_js.js
+++ app_js.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
    $("#button").click(function() {
     $("#redball")
-        .animate({right: "400"},2000)
-        .animate({left: "400"},2000)
+        .animate({right: "249"},2000)
+        .animate({left:"249"},2000)
      });
 });
 
